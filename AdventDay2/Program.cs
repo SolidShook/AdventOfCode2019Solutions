ab91c8deb49b3f93bdee44ff4be2a4a34d15a86b
SolidShook <SolidShook@outlook.com>
2019-12-04 20:09:31 +0000

    initial commit

---
+++ AdventDay2/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace AdventDay2
+{
+    class Program
+    {
+        private static void ProcessRow(int[] row)
+        {
+
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            int counter = 0;
+            string line;
+
+            System.IO.StreamReader file =
+                new System.IO.StreamReader("../../../data.txt");
+
+            string str = file.ReadToEnd();
+
+            List<int[]> rows = new List<int[]> ();
+            string[] strArray = str.Split(',');
+
+            for (int i = 0; i < strArray.Length; i+=5)
+            {
+                int[] row = new int[5];
+                for (int j = 0; j < 5; j++)
+                {
+                    row[j] = Int32.Parse(strArray[i+j]);
+                }
+
+                rows.Add(row);
+            }
+
+            foreach (int[] row in rows)
+            {
+                
+            }
+
+            file.Close();
+            System.Console.WriteLine("There were {0} lines.", counter);
+            // Suspend the screen.  
+            System.Console.ReadLine();
+        }
+    }
+}
---
+++ ChristmasChallenge/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+
+namespace ChristmasChallenge
+{
+    class Program
+    {
+        private static int CalcThing(int mass)
+        {
+            int fuel = 0;
+            fuel = (int)Math.Floor((float)(mass / 3));
+            fuel -= 2;
+
+            return fuel;
+        }
+        private static int CalculateFuel(int mass)
+        {
+            int fuel = CalcThing(mass);
+
+            if (CalcThing(fuel) > 0)
+            {
+                fuel += CalculateFuel(fuel);
+            }
+
+            return fuel;
+        }
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            int counter = 0;
+            string line;
+
+            int fuel = 0;
+
+            System.IO.StreamReader file =
+                new System.IO.StreamReader("../../../data.txt");
+            while ((line = file.ReadLine()) != null)
+            {
+                System.Console.WriteLine(line);
+                fuel += CalculateFuel(Int32.Parse(line));
+                counter++;
+            }
+
+            file.Close();
+            System.Console.WriteLine("There were {0} lines.", counter);
+            System.Console.WriteLine("You need {0} fuel", fuel);
+            // Suspend the screen.  
+            System.Console.ReadLine();
+        }
+    }
+}
