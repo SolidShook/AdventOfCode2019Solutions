d2a09be8b420757f3eef6af6c22d1c9665ddb603
SolidShook <SolidShook@outlook.com>
2019-12-05 17:00:19 +0000

    Complete 4 using regex

--- AdventDay4/Program.cs
+++ AdventDay4/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Text.RegularExpressions;
 
 namespace AdventDay4
 {
@@ -31,6 +32,7 @@ namespace AdventDay4
                 if (value[i + 1] < value[i])
                 {
                     decrease = true;
+                    return false;
                 }
             }
 
@@ -40,6 +42,92 @@ namespace AdventDay4
             }
 
             return true;
+        }
+
+        //public bool attempt2(string value, int min, int max)
+        //{
+        //    if (value.Length < 6)
+        //    {
+        //        return false;
+        //    }
+
+        //    int intValue = int.Parse(value);
+        //    if (intValue < min || intValue > max)
+        //    {
+        //        return false;
+        //    }
+
+        //    int matchCounts = 0;
+        //    bool decrease = false;
+        //    for (var i = 0; i < value.Length - 1; i++)
+        //    {
+        //        if (value[i + 1] < value[i])
+        //        {
+        //            decrease = true;
+        //            return false;
+        //        }
+
+        //        if (i > 0)
+        //        {
+        //            if (value[i - 1] > value[i])
+        //            {
+        //                decrease = true;
+        //                return false;
+        //            }
+        //        }
+
+        //        int lookahead = 1;
+        //        int match = 0;
+        //        int space = value.Length - i;
+
+        //        do
+        //        {
+        //            int c1 = value[i + lookahead] - '0';
+        //            int c2 = value[i] - '0';
+        //            if (c1 == c2)
+        //            {
+        //                match++;
+        //                lookahead++;
+        //            }
+        //            else
+        //            {
+        //                space = 0;
+        //            }
+        //        } while (lookahead < space && match > 0);
+
+        //        if (match == 1)
+        //        {
+        //            matchCounts++;
+        //        }
+
+        //        if (match > 1)
+        //        {
+        //            i = i + lookahead - 1;
+        //        }
+        //    }
+
+        //    if (matchCounts == 0 || decrease)
+        //    {
+        //        return false;
+        //    }
+
+        //    return true;
+        //}
+
+        public bool part2(string value)
+        {
+            int matchCount = 0;
+
+            Regex regex = new Regex(@"(1+|2+|3+|4+|5+|6+|7+|8+|9+|0+)");
+            foreach (Match match in regex.Matches(value))
+            {
+                if (match.Length == 2)
+                {
+                    matchCount++;
+                }
+            };
+
+            return matchCount > 0;
         }
         public Password()
         {
@@ -58,19 +146,25 @@ namespace AdventDay4
             int max = 576723;
             int iterator = min;
             int finds = 0;
+
             while (iterator <= max)
             {
-                string attempt = min.ToString();
+                string attempt = iterator.ToString();
 
-                if (password.attempt(iterator.ToString(), min, max))
+                if (password.attempt(attempt, min, max))
                 {
-                    finds++;
+                    if (password.part2(attempt))
+                    {
+                        Console.WriteLine("Passed: {0}", iterator.ToString());
+                        finds++;
+                    }
                 }
 
                 iterator++;
             }
 
             Console.WriteLine("Found passwords count: {0}", finds);
+
             System.Console.ReadLine();
         }
     }
