882b16ec1ebcec7d80f0aa6c536a60c1992b9221
SolidShook <SolidShook@outlook.com>
2019-12-09 14:47:23 +0000

    AdventDay8 donezo

--- AdventDay8/Program.cs
+++ AdventDay8/Program.cs
@@ -1,14 +1,24 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
+using System.Runtime.InteropServices.ComTypes;
+using System.Text;
 using Microsoft.VisualBasic;
 
 namespace AdventDay8
 {
     class Layer
     {
+        //this could be in bytes if efficiency is wanted
         public char[] Data;
+        public int Height;
+        public int Width;
 
+        public char GetValueAtIndex(int x, int y)
+        {
+            return Data[x + y * Width];
+        }
 
         public int GetNumberOfZeros()
         {
@@ -46,22 +56,39 @@ namespace AdventDay8
             return oneCount * twoCount;
         }
 
-        public Layer(char[] data)
+        public char[][] GetJaggedArray()
+        {
+            List<char[]> rows = new List<char[]>();
+
+            for (int y = 0; y < Height; y++)
+            {
+                char[] row = Data.Skip(y * Width).Take(Width).ToArray();
+                rows.Add(row);
+            }
+
+            return rows.ToArray();
+        }
+
+        public Layer(char[] data, int width, int height)
         {
             Data = data;
+            Height = height;
+            Width = width;
         }
     }
 
     class ImageReader
     {
-        private int FindLayerWithFewestZeros(Layer[] layers)
+        public Layer[] Layers;
+
+        private int FindLayerWithFewestZeros()
         {
             int? leastZeroCount = null;
             int leastAddress = 0;
 
-            for (int x = 0; x < layers.Length; x++)
+            for (int x = 0; x < Layers.Length; x++)
             {
-                int zeroCount = layers[x].GetNumberOfZeros();
+                int zeroCount = Layers[x].GetNumberOfZeros();
 
                 if (leastZeroCount > zeroCount || leastZeroCount == null)
                 {
@@ -73,7 +100,7 @@ namespace AdventDay8
             return leastAddress;
         }
 
-        private Layer[] GetLayers(string str, int width, int height)
+        private void SetLayers(string str, int width, int height)
         {
             int layerLength = width * height;
             int numberOfLayers = str.Length / layerLength;
@@ -81,22 +108,69 @@ namespace AdventDay8
             List<Layer> layers = new List<Layer>();
             for (int x = 0; x < numberOfLayers; x++)
             {
-                layers.Add(new Layer(str.Skip(x * layerLength).Take(layerLength).ToArray()));
+                layers.Add(new Layer(str.Skip(x * layerLength).Take(layerLength).ToArray(), width, height));
             }
 
-            return layers.ToArray();
+            Layers = layers.ToArray();
         }
 
-        public int Process(string str, int width, int height)
+        public int Process()
         {
-            Layer[] layers = GetLayers(str, width, height);
-
-            return layers[FindLayerWithFewestZeros(layers)].OnesMultipliedByTwos();
+            return Layers[FindLayerWithFewestZeros()].OnesMultipliedByTwos();
         }
 
-        public ImageReader()
+        public Layer FindCode()
         {
+            int width = Layers[0].Width;
+            int height = Layers[0].Height;
 
+            char[] data = new char[height * width];
+
+            for (int y = 0; y < height; y++)
+            {
+                for (int x = 0; x < width; x++)
+                {
+                    char finalRowValue = 'f';
+
+                    foreach (Layer layer in Layers)
+                    {
+                        char rowValue = layer.GetValueAtIndex(x, y);
+
+                        if (rowValue == '0')
+                        {
+                            finalRowValue = ' ';
+                            break;
+                        }
+
+                        if (rowValue == '1')
+                        {
+                            finalRowValue = (char) 380;
+                            break;
+                        }
+                    }
+
+                    data[x + y * width] = finalRowValue;
+                }
+            }
+
+            return new Layer(data, width, height);
+        }
+
+        public void GetAnswer2()
+        {
+            Layer layer = FindCode();
+
+            char[][] jagged = layer.GetJaggedArray();
+
+            foreach (char[] thing in jagged)
+            {
+                Console.WriteLine(new string(thing));
+            }
+        }
+
+        public ImageReader(string str, int width, int height)
+        {
+            SetLayers(str, width, height);
         }
     }
 
@@ -108,14 +182,14 @@ namespace AdventDay8
                 new System.IO.StreamReader("../../../data.txt");
 
             string str = file.ReadToEnd();
-
             file.Close();
 
             while (true)
             {
-                ImageReader imageReader = new ImageReader();
-                int answer = imageReader.Process(str, 25, 6);
+                ImageReader imageReader = new ImageReader(str, 25, 6);
+                int answer = imageReader.Process();
 
+                imageReader.GetAnswer2();
                 Console.WriteLine("question 1 answer {0}", answer);
                 Console.ReadLine();
             }
