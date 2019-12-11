a903ef992896ab31becbc47e2aa09f6420c68b4b
SolidShook <SolidShook@outlook.com>
2019-12-11 13:33:59 +0000

    Question 10 example

---
+++ AdventDay10/Program.cs
@@ -0,0 +1,249 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Security.Cryptography.X509Certificates;
+using Microsoft.VisualBasic.CompilerServices;
+
+namespace AdventDay10
+{
+    class Point
+    {
+        public int X;
+        public int Y;
+
+        public override bool Equals(Object obj)
+        {
+            if ((obj == null) || !this.GetType().Equals(obj.GetType()))
+            {
+                return false;
+            }
+            else
+            {
+                Point p = (Point)obj;
+                return (X == p.X) && (Y == p.Y);
+            }
+        }
+
+        public override int GetHashCode()
+        {
+            return (X << 2) ^ Y;
+        }
+
+        public Point()
+        {
+
+        }
+        public Point(int x, int y)
+        {
+            X = x;
+            Y = y;
+        }
+    }
+    class Asteroid
+    {
+        public Point Pos;
+        public double AngleX;
+        public double AngleY;
+
+        public override bool Equals(Object obj)
+        {
+            if ((obj == null) || !this.GetType().Equals(obj.GetType()))
+            {
+                return false;
+            }
+            else
+            {
+                Asteroid p = (Asteroid)obj;
+
+                return AngleX == p.AngleX && AngleY == p.AngleY;
+            }
+        }
+
+        public static bool operator ==(Asteroid lhs, Asteroid rhs)
+        {
+            return lhs.Equals(rhs);
+        }
+
+        public static bool operator !=(Asteroid lhs, Asteroid rhs)
+        {
+            return !(lhs.Equals(rhs));
+        }
+
+        public override int GetHashCode()
+        {
+            return (int) AngleY ^ (int)AngleX;
+        }
+
+        public Asteroid(int x, int y)
+        {
+            Pos = new Point(x, y);
+
+            double m = Math.Sqrt(x * x + y * y);
+
+            AngleX = Math.Round(x / m, 2);
+            AngleY = Math.Round(y / m, 2);
+        }
+    }
+    class Map
+    {
+
+        private List<string> _map = new List<string>();
+
+        private void CompareAsteroids()
+        {
+
+        }
+
+        private int ScanAroundAsteroid(Point centre)
+        {
+            bool leftReached = false, rightReached = false, topReached = false, bottomReached = false;
+
+            int dist = 1;
+
+            HashSet<Asteroid> asts = new HashSet<Asteroid>();
+
+            //void CheckBoundaries()
+            //{
+            //    for (int y = -dist; y <= dist; y++)
+            //    {
+            //        if (centre.Y + y < 0)
+            //        {
+            //            topReached = true;
+            //            continue;
+            //        }
+            //        else if (centre.Y + y > _map.Count - 1 - centre.Y)
+            //        {
+            //            bottomReached = true;
+            //            continue;
+            //        }
+
+            //        for (int x = -dist; x <= dist; x++)
+            //        {
+            //            int place = centre.Y + y;
+            //            if (centre.X + x < 0)
+            //            {
+            //                leftReached = true;
+            //            } else if (centre.X + x < _map[centre.Y + y].Length - 1 - centre.X)
+            //            {
+            //                rightReached = true;
+            //            }
+            //        }
+            //    }
+            //}
+
+            void AnalyseAsteroid(int localX, int localY)
+            {
+                Asteroid ast = new Asteroid(localX, localY);
+                
+                asts.Add(ast);
+            }
+
+            void CheckPoints()
+            {
+                for (int y = -dist; y <= dist; y++)
+                {
+                    int globalY = centre.Y + y;
+                    if (globalY < 0)
+                    {
+                        topReached = true;
+                        continue;
+                    }
+                    else if (globalY > _map.Count - 1)
+                    {
+                        bottomReached = true;
+                        continue;
+                    }
+
+                    for (int x = -dist; x <= dist; x++)
+                    {
+                        int globalX = centre.X + x;
+
+                        if (globalX == centre.X && globalY == centre.Y)
+                        {
+                            continue;
+                        }
+
+                        if (globalX < 0)
+                        {
+                            leftReached = true;
+                            continue;
+                        }
+
+                        if (globalX > _map[globalY].Length - 1)
+                        {
+                            rightReached = true;
+                            continue;
+                        }
+
+                        if (_map[globalY][globalX] == '#')
+                        {
+                            AnalyseAsteroid(x, y);
+                        }
+                    }
+                }
+            }
+
+            while (!leftReached || !rightReached || !topReached || !bottomReached)
+            {
+                CheckPoints();
+                dist++;
+            }
+
+            return asts.Count;
+        }
+
+        public void FindBestAsteroid()
+        {
+            int mostFound = 0;
+            int foundX = 0;
+            int foundY = 0;
+
+            for (int y = 0; y < _map.Count; y++)
+            {
+                for (int x = 0; x < _map[y].Length; x++)
+                {
+                    if (_map[y][x] == '#')
+                    {
+                        int ast = ScanAroundAsteroid(new Point(x, y));
+                        
+                        if (ast > mostFound)
+                        {
+                            mostFound = ast;
+                            foundX = x;
+                            foundY = y;
+                        }
+                    }
+                }
+            }
+
+
+            int pee = 0;
+        }
+
+        public Map(string fileAddress)
+        {
+            System.IO.StreamReader file =
+                new System.IO.StreamReader(fileAddress);
+
+            string line;
+
+            while ((line = file.ReadLine()) != null)
+            {
+                _map.Add(line);
+            }
+
+            FindBestAsteroid();
+
+            file.Close();
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            Map testmap = new Map("../../../test1.txt");
+        }
+    }
+}
